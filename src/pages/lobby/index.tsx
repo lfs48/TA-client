a6371ba6c89b8812ba722673a43a7167ba9b2c45
Lucas Schraier <lucas.schraier@shipsigma.com>
2025-06-12 12:06:55 -0400

    style: remove unused deps

--- src/pages/lobby/index.tsx
+++ src/pages/lobby/index.tsx
@@ -1,36 +1,21 @@
 import { skipToken } from '@reduxjs/toolkit/query';
 import { useSelector } from 'react-redux';
-import { useState } from 'react';
 
 import { useGetUserGamesQuery } from '@/api/game.api';
 import { RootState } from '@/types';
 import GameSkeleton from './game-skeleton';
 import GameCard from './game-card';
 import LobbyControls from './lobby-controls';
-import { LobbyFilterOptions } from '@/enum';
-import LobbyContext from './lobby-context';
 import { selectGames } from '@/reducers/entities/games.reducer';
 
 export default function Lobby() {
-
-    const [filter, setFilter] = useState(LobbyFilterOptions.ALL);
 
     const userId = useSelector((state:RootState) => state.session.id);
     const games = useSelector((state: RootState) => selectGames(state));
 
     const { isLoading } = useGetUserGamesQuery(userId ?? skipToken);
 
-    const filteredGames = (filter === LobbyFilterOptions.ALL) ? games : games
-    .filter((game) => {
-        switch (filter) {
-            case(LobbyFilterOptions.PLAYING):
-                return game.players.some(player => player.id === userId)
-            case (LobbyFilterOptions.GMING):
-                return game.gm.id === userId;
-        }
-    });
-
-    const gameList = filteredGames.map((game, i) => (
+    const gameList = games.map((game, i) => (
         <GameCard key={i} game={game}/>
     ));
 
