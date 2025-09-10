b17656e8f9446089e84a50a80a76851982fe0a9b
Lucas Schraier <lucas.schraier@shipsigma.com>
2025-09-09 20:50:53 -0400

    refactor: remove unused declarations and imports

--- src/components/agent/sheet/currency/agent-currency.tsx
+++ src/components/agent/sheet/currency/agent-currency.tsx
@@ -1,12 +1,6 @@
 import { useAppSelector } from "@/hooks/useAppSelector.hook";
 import { selectAgentById } from "@/reducers/entities/agent.reducer";
-import { Agent, Currency } from "@/types";
-import { 
-    useEarnCurrencyMutation, 
-    useResetAgentCurrencyMutation, 
-    useSpendCurrencyMutation 
-} from "@/api/agent.api";
-import { useState } from "react";
+import { Agent } from "@/types";
 import CurrencySection from "./currency-section";
 
 interface AgentCurrencyProps {
@@ -19,31 +13,6 @@ export default function AgentCurrency({
 
     const agent = useAppSelector(state => selectAgentById(state, id)) as Agent;
     const {commendations, demerits} = agent?.currency;
-
-    const [commInput, setCommInput] = useState(0);
-    const [demInput, setDemInput] = useState(0);
-
-    const [triggerEarn, {isLoading: earnLoading}] = useEarnCurrencyMutation();
-    const [triggerSpend, {isLoading: spendLoading}] = useSpendCurrencyMutation();
-    const [triggerReset, {isLoading: resetLoading}] = useResetAgentCurrencyMutation();
-
-    const loading = earnLoading || spendLoading || resetLoading;
-
-    const handleTransaction = async (type: 'earn' | 'spend', currency: Currency, quantity: number) => {
-        const data = { currency, quantity };
-        if (type === 'earn') {
-            await triggerEarn({ id, data });
-        } else {
-            await triggerSpend({ id, data });
-        }
-        currency === 'commendations' ? setCommInput(0) : setDemInput(0);
-    };
-
-    const handleReset = async (currency: Currency) => {
-        if (!loading) {
-            await triggerReset({ id, data: { currency } });
-        }
-    };
 
     return (
         <div className="flex flex-col space-y-2">
